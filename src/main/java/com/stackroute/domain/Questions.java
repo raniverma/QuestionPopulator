a1617bc9e6ec0e7c009da5a2034ce49e1f991298
pratima.verma <pratima.verma@boeing.com>
2019-02-12 18:16:20 +0530

    final commit

--- src/main/java/com/stackroute/domain/Questions.java
+++ src/main/java/com/stackroute/domain/Questions.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.annotation.Id;
 
 @Data
-@Document
+@Document(collection="questions")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -24,7 +24,8 @@ public class Questions{
     @Field
     private String gitUrl;
 
-    public double getQuestionId() {
+
+    public int getQuestionId() {
         return questionId;
     }
 
--- src/main/java/com/stackroute/service/QuestionServiceImpl.java
+++ src/main/java/com/stackroute/service/QuestionServiceImpl.java
@@ -18,15 +18,9 @@ public class QuestionServiceImpl implements QuestionService {
 
     @Override
     public Questions saveQuestion(Questions question) throws QuestionAlreadyExistsException{
-        System.out.println(question);
-        MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-        DB db = mongoClient.getDB("admin11");
-        DBCollection collection = db.getCollection("counters");
         BasicDBObject document = new BasicDBObject();
         document.put("_id", getNextSequence("questionId"));
-        collection.insert(document);
         question.setQuestionId((int)document.get("_id")+1);
-      //  System.out.println("pratima hghjsgfjhdgsfjgdj"+(int)question.getQuestionId());
 
         if(questionRepository.existsById((int)(question.getQuestionId()))) {
             throw new QuestionAlreadyExistsException("This Question already exists");
@@ -37,14 +31,9 @@ public class QuestionServiceImpl implements QuestionService {
 
     public static Object getNextSequence(String name){
         MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-        DB db = mongoClient.getDB("admin11");
-        db.createCollection("counters", null);
+        DB db = mongoClient.getDB("mashup");
         DBCollection collection = db.getCollection("counters");
-
-        /*Remove first document from collection*/
         BasicDBObject find = new BasicDBObject();
-        /*Insert document into collection*/
-      //  find.put("_id", name);
         BasicDBObject update = new BasicDBObject();
         update.put("$inc", new BasicDBObject("seq", 1));
         DBObject obj =  collection.findAndModify(find, update);
